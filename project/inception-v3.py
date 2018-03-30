e69abe55fb8125333ba8a9429b7cc5d37fad3035
yetongxue <xianyong.ye@monstar-lab.com>
2018-03-30 18:24:24 +0800

    inception

---
+++ project/inception-v3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# inception-v3.py in machine_learn
+# Created by yetongxue at 2018/3/30 18:12
+
+
+#http://download.tensorflow.org/models/image/imagenet/inception-2015-12-05.tgz
+
+INCEPTION_PATH='/Users/yetongxue/Downloads/inception-2015-12-05'
+
