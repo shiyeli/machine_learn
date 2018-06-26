7eecd695e8e0f904643dbe3ddddfc13129824c1a
yetongxue <xianyong.ye@monstar-lab.com>
2018-06-26 22:22:40 +0800

    format

--- tensorflow_1.4.0/LeNet5_mnist_train.py
+++ tensorflow_1.4.0/LeNet5_mnist_train.py
@@ -25,40 +25,40 @@ MODEL_NAME = 'model.ckpt'
 
 def train(mnist):
     x = tf.placeholder(
-            tf.float32,
-            [None, mnist_inference.IMAGE_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.NUM_CHANNELS],
-            name='x-input'
+        tf.float32,
+        [None, mnist_inference.IMAGE_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.NUM_CHANNELS],
+        name='x-input'
     )
     y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
-    
+
     regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE)
-    
+
     # 向前传播
     y = mnist_inference.inference(x, True, regularizer)
     global_step = tf.Variable(0, trainable=False)
-    
+
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
     variable_averages_op = variable_averages.apply(tf.trainable_variables())
-    
+
     cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
     cross_entropy_mean = tf.reduce_mean(cross_entropy)
-    
+
     loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
     learning_rate = tf.train.exponential_decay(
-            LEARNING_RATE_BASE,
-            global_step,
-            mnist.train.num_examples / BATCH_SIZE,
-            LEARNING_RATE_DECAY
+        LEARNING_RATE_BASE,
+        global_step,
+        mnist.train.num_examples / BATCH_SIZE,
+        LEARNING_RATE_DECAY
     )
     train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
-    
+
     with tf.control_dependencies([train_step, variable_averages_op]):
         train_op = tf.no_op(name='train')
-    
+
     saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        
+
         for i in range(TRAINING_STEPS):
             xs, ys = mnist.train.next_batch(BATCH_SIZE)
             reshaped_xs = np.reshape(xs, [
@@ -68,11 +68,11 @@ def train(mnist):
                 mnist_inference.NUM_CHANNELS
             ])
             _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})
-            
+
             # 每1000轮保存一次模型
             if i % 1000 == 0:
                 print 'After %d training steps,loss=%f' % (step, loss_value)
-                
+
                 # 保存模型
                 saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=step)
 
@@ -89,6 +89,5 @@ if __name__ == '__main__':
     第一次运行将下行代码注释，以后运行放开
     """
     # tf.get_variable_scope().reuse_variables()
-    
-    tf.app.run()
 
+    tf.app.run()
