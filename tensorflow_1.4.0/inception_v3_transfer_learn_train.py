2b4c726f4d9161edf17e3858c0ecd44e637aecfc
叶同学 <xianyong.ye@monstar-lab-cn.com>
2018-06-29 17:58:46 +0800

    tmp

--- tensorflow_1.4.0/inception_v3_transfer_learn_train.py
+++ tensorflow_1.4.0/inception_v3_transfer_learn_train.py
@@ -45,42 +45,15 @@ def train(datasets,test_datasets):
             xs, ys = datasets.next_batch(BATCH)
             sess.run(train_step, feed_dict={x : xs,y_: ys })
             
-            if i % 50 == 0:
+            if i % 100 == 0:
                 loss_eval = sess.run(loss, feed_dict={x: xs, y_: ys})
-                print 'Step:%d, loss=%f' % (i, loss_eval)
-            
-            if i%100==0:
-                accuracy_score = sess.run(accuracy, feed_dict={x: test_datasets.datasets[0], y_: test_datasets.datasets[1]})
-                print 'Accuracy score is %f' % accuracy_score
-            
+                accuracy_score = sess.run(accuracy,feed_dict={x: test_datasets.datasets[0], y_: test_datasets.datasets[1]})
+                print 'Step:%d, loss=%f, accuracy=%f' % (i, loss_eval,accuracy_score)
+                
             # 保存模型
-            saver.save(sess, MODE_SAVE_PATH, global_step=i)
+            # saver.save(sess, MODE_SAVE_PATH, global_step=i)
             
             
 if __name__ == '__main__':
     train(datasets.Datasets(is_train=True),datasets.Datasets(is_train=False))
     
-    
-    
-    """
-    Step:0, loss=2.946330
-    Step:50, loss=2.670017
-    Step:100, loss=2.195953
-    Step:150, loss=2.900992
-    Step:200, loss=2.670817
-    Step:250, loss=2.562477
-    Step:300, loss=2.841588
-    Step:350, loss=2.520652
-    Step:400, loss=2.789753
-    Step:450, loss=2.374507
-    Step:500, loss=2.459342
-    Step:550, loss=2.388236
-    Step:600, loss=2.315579
-    Step:650, loss=2.442888
-    Step:700, loss=2.303853
-    Step:750, loss=2.536525
-    Step:800, loss=2.397790
-    Step:850, loss=2.455190
-    Step:900, loss=2.287944
-    Step:950, loss=2.434917
-    """
